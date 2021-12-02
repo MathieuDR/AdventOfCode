e17b2408804950bde2e6c6f4042e679bf41adeb8
Mathieu De Raedt <mathieu@deraedt.dev>
2021-12-02 11:52:21 +0100

    Day 3, 2020 part 1

---
+++ Tests/Year_2020/Day03Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using AdventOfCode.Year_2020;
+
+namespace Tests.Year_2020;
+
+public class Day03Tests {
+    [Fact]
+    public async Task IsValidTobogganPassword() {
+        var input =
+            "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#";
+
+        var day = new Day03(input);
+        var solve = await day.Solve_1();
+
+        solve.Should().Be("We hit 7 trees");
+    }
+}
---
+++ Year_2020/Day03.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.Year_2020;
+
+/// <summary>
+///     Day 2 from year 2020
+/// </summary>
+public class Day03 : BaseDay {
+    private readonly string[] map;
+
+    public Day03() {
+        map = File.ReadAllLines(InputFilePath);
+    }
+
+    public Day03(string input) {
+        map = input.Split("\n");
+    }
+
+    private int CalculateCollisions(int rightStep, int downStep) {
+        var collisions = 0;
+        for (var i = 0; i < map.Length; i += downStep) {
+            var index = rightStep * i % map[i].Length;
+            var mapObject = map[i][index];
+            if (mapObject == '#') {
+                collisions++;
+            }
+        }
+
+        return collisions;
+    }
+
+    public override ValueTask<string> Solve_1() {
+        var collisions = CalculateCollisions(3, 1);
+        return new ValueTask<string>(
+            $"We hit {collisions} trees");
+    }
+
+    public override ValueTask<string> Solve_2() {
+        return new ValueTask<string>(
+            "");
+    }
+}
