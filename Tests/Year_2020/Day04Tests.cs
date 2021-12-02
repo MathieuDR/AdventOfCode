1f9d3ffa55e4dbcfb6d14f5ca788ca85837f90c3
Mathieu De Raedt <mathieu@deraedt.dev>
2021-12-02 13:07:23 +0100

    part 2 day 4 2020

---
+++ Tests/Year_2020/Day04Tests.cs
@@ -0,0 +1,109 @@
+﻿using AdventOfCode.Year_2020;
+
+namespace Tests.Year_2020;
+
+public class Day04Tests {
+    [Fact]
+    public void NumberBetweenReturnsFalseForOverTheLimit() {
+        var result = Day04.Passport.NumberBetween("2919", 1920, 2020);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void NumberBetweenReturnsFalseForUnderTheLimit() {
+        var result = Day04.Passport.NumberBetween("1900", 1920, 2020);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void NumberBetweenReturnsTrueForOnTheBottomLimit() {
+        var result = Day04.Passport.NumberBetween("1920", 1920, 2020);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void NumberBetweenReturnsTrueForOnTheUpperLimit() {
+        var result = Day04.Passport.NumberBetween("2020", 1920, 2020);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void NumberBetweenReturnsTrueForInLimit() {
+        var result = Day04.Passport.NumberBetween("2000", 1920, 2020);
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("190", false)]
+    [InlineData("astst", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    [InlineData("arstascm", false)]
+    [InlineData("70bs", false)]
+    [InlineData("140cm", false)]
+    [InlineData("200cm", false)]
+    [InlineData("150cm", true)]
+    [InlineData("193cm", true)]
+    [InlineData("183cm", true)]
+    [InlineData("50in", false)]
+    [InlineData("100in", false)]
+    [InlineData("59in", true)]
+    [InlineData("76in", true)]
+    [InlineData("70in", true)]
+    public void HeightValidationIsCorrect(string height, bool valid) {
+        var result = Day04.Passport.ValidateHeight(height);
+
+        result.Should().Be(valid);
+    }
+
+    [Theory]
+    [InlineData("190", false)]
+    [InlineData("astst", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    [InlineData("arstascm", false)]
+    [InlineData("70bs", false)]
+    [InlineData("#rstrst", false)]
+    [InlineData("#0000", false)]
+    [InlineData("#000p00", false)]
+    [InlineData("#000000", true)]
+    [InlineData("#00a09f", true)]
+    [InlineData("000000", false)]
+    public void HairColorValidationIsCorrect(string hair, bool valid) {
+        var result = Day04.Passport.ValidateHair(hair);
+
+        result.Should().Be(valid);
+    }
+
+    [Theory]
+    [InlineData("brown", false)]
+    [InlineData("astst", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    [InlineData("arstascm", false)]
+    [InlineData("70bs", false)]
+    [InlineData("#rstrst", false)]
+    [InlineData("#0000", false)]
+    [InlineData("#000p00", false)]
+    [InlineData("amb", true)]
+    [InlineData("blu", true)]
+    [InlineData("brn", true)]
+    [InlineData("gry", true)]
+    [InlineData("grn", true)]
+    [InlineData("hzl", true)]
+    [InlineData("oth", true)]
+    public void EyeColorValidationIsCorrect(string eyeColor, bool valid) {
+        var result = Day04.Passport.ValidateEyes(eyeColor);
+
+        result.Should().Be(valid);
+    }
+
+    [Theory]
+    [InlineData("000000001", true)]
+    [InlineData("0123456789", false)]
+    public void PassportIdValidationIsCorrect(string eyeColor, bool valid) {
+        var result = Day04.Passport.ValidatePid(eyeColor);
+
+        result.Should().Be(valid);
+    }
+}
---
+++ Year_2020/Day04.cs
@@ -0,0 +1,150 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year_2020;
+
+public class Day04 : BaseDay {
+    private readonly Passport[] _passports;
+
+    public Day04() {
+        _passports = File.ReadAllText(InputFilePath).Split("\r\n\r\n").Select(x => new Passport(x)).ToArray();
+    }
+
+    public override ValueTask<string> Solve_1() {
+        return new ValueTask<string>(
+            $"There are {_passports.Count(x => x.IsValidSimple())} valid passports");
+    }
+
+    public override ValueTask<string> Solve_2() {
+        return new ValueTask<string>(
+            $"There are {_passports.Count(x => x.IsValid())} valid passports");
+    }
+
+    public class Passport {
+        public Passport(string record) {
+            var lines = record.Split("\r\n");
+
+            foreach (var line in lines) {
+                var fields = line.Split(" ");
+                foreach (var field in fields) {
+                    var info = field.Split(":");
+
+                    switch (info[0]) {
+                        case "byr":
+                            BirthYear = info[1];
+                            break;
+                        case "iyr":
+                            IssueYear = info[1];
+                            break;
+                        case "eyr":
+                            ExpirationYear = info[1];
+                            break;
+                        case "hgt":
+                            Height = info[1];
+                            break;
+                        case "hcl":
+                            HairColor = info[1];
+                            break;
+                        case "ecl":
+                            EyeColor = info[1];
+                            break;
+                        case "pid":
+                            PassportId = info[1];
+                            break;
+                        case "cid":
+                            CountryId = info[1];
+                            break;
+                    }
+                }
+            }
+        }
+
+        public string BirthYear { get; set; }
+        public string IssueYear { get; set; }
+        public string ExpirationYear { get; set; }
+        public string Height { get; set; }
+        public string HairColor { get; set; }
+        public string EyeColor { get; set; }
+        public string PassportId { get; set; }
+        public string CountryId { get; set; }
+
+        public static bool NumberBetween(string number, int min, int max) {
+            if (string.IsNullOrEmpty(number) || !int.TryParse(number, out var @int)) {
+                return false;
+            }
+
+            return @int >= min && @int <= max;
+        }
+
+        private static readonly Regex HeightRegex = new("(\\d+)(cm|in)", RegexOptions.Compiled);
+        private static readonly Regex HairRegex = new("#[0-9a-f]{6}", RegexOptions.Compiled);
+        private static readonly Regex EyeRegex = new("(amb|blu|brn|gry|grn|hzl|oth)", RegexOptions.Compiled);
+        private static readonly Regex PidRegex = new("^\\d{9}$", RegexOptions.Compiled);
+
+        public static bool ValidateHeight(string height) {
+            if (string.IsNullOrEmpty(height)) {
+                return false;
+            }
+
+            var matches = HeightRegex.Matches(height);
+            if (matches.Count != 1) {
+                return false;
+            }
+
+            if (!int.TryParse(matches[0].Groups[1].Value, out var heightInt)) {
+                return false;
+            }
+
+            return matches[0].Groups[2].Value switch {
+                "cm" => NumberBetween(matches[0].Groups[1].Value, 150, 193),
+                "in" => NumberBetween(matches[0].Groups[1].Value, 59, 76),
+                _ => false
+            };
+        }
+
+        public static bool ValidateHair(string hairColor) {
+            if (string.IsNullOrEmpty(hairColor)) {
+                return false;
+            }
+
+            return HairRegex.IsMatch(hairColor);
+        }
+
+        public static bool ValidatePid(string pid) {
+            if (string.IsNullOrEmpty(pid)) {
+                return false;
+            }
+
+            return PidRegex.IsMatch(pid);
+        }
+
+        public static bool ValidateEyes(string eyes) {
+            if (string.IsNullOrEmpty(eyes)) {
+                return false;
+            }
+
+            return EyeRegex.IsMatch(eyes);
+        }
+
+        public bool IsValidSimple() {
+            return
+                !string.IsNullOrEmpty(BirthYear) &&
+                !string.IsNullOrEmpty(IssueYear) &&
+                !string.IsNullOrEmpty(ExpirationYear) &&
+                !string.IsNullOrEmpty(Height) &&
+                !string.IsNullOrEmpty(HairColor) &&
+                !string.IsNullOrEmpty(EyeColor) &&
+                !string.IsNullOrEmpty(PassportId);
+        }
+
+        public bool IsValid() {
+            return
+                NumberBetween(BirthYear, 1920, 2002) &&
+                NumberBetween(IssueYear, 2010, 2020) &&
+                NumberBetween(ExpirationYear, 2020, 2030) &&
+                ValidateHeight(Height) &&
+                ValidateHair(HairColor) &&
+                ValidateEyes(EyeColor) &&
+                ValidatePid(PassportId);
+        }
+    }
+}
