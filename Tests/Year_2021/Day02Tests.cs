85881d1a78e0c59bf6489fb6f4e1f77671eb3fcc
Mathieu De Raedt <mathieu@deraedt.dev>
2021-12-02 09:24:46 +0100

    Day 2, Part 1

---
+++ Tests/Year_2021/Day02Tests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Year_2021;
+
+namespace Tests.Year_2021;
+
+public class Day02Tests {
+    [Fact]
+    public void CalculateDepthIsCorrect() {
+        var input = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2";
+
+
+        var (depth, _) = Day02.CalculatePosition(input.Split("\n"));
+        depth.Should().Be(10);
+    }
+
+    [Fact]
+    public void CalculateHorizontalIsCorrect() {
+        var input = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2";
+
+
+        var (_, horizontal) = Day02.CalculatePosition(input.Split("\n"));
+        horizontal.Should().Be(15);
+    }
+}
---
+++ Year_2021/Day02.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year_2021;
+
+/// <summary>
+///     Day 1 from year 2021
+/// </summary>
+public class Day02 : BaseDay {
+    private readonly string[] _commands;
+    private static readonly Regex Regex = new("(\\w+) (\\d+)", RegexOptions.Compiled);
+
+    public Day02() {
+        _commands = File.ReadAllLines(InputFilePath).ToArray();
+    }
+
+    public static (int depth, int horizontal) CalculatePosition(string[] commands) {
+        int depth = 0, horizontal = 0;
+        foreach (var command in commands) {
+            var matches = Regex.Matches(command);
+            var dir = matches[0].Groups[1].Value;
+            var amount = int.Parse(matches[0].Groups[2].Value);
+
+            switch (dir) {
+                case "forward":
+                    horizontal += amount;
+                    break;
+                case "back":
+                    horizontal -= amount;
+                    break;
+                case "down":
+                    depth += amount;
+                    break;
+                case "up":
+                    depth -= amount;
+                    break;
+            }
+        }
+
+        return (depth, horizontal);
+    }
+
+    public override ValueTask<string> Solve_1() {
+        var (depth, horizontal) = CalculatePosition(_commands);
+
+        return new ValueTask<string>($"The multiplication is {depth * horizontal}");
+    }
+
+    public override ValueTask<string> Solve_2() {
+        return new ValueTask<string>("");
+    }
+}
