fb95b0b7edaea778bc9947f1a21679dadef20528
Mathieu De Raedt <mathieu@deraedt.dev>
2021-12-05 14:22:49 +0100

    Small refactor

--- Year_2021/Day05.cs
+++ Year_2021/Day05.cs
@@ -42,33 +42,51 @@ public class Day05 : BaseDay {
             foreach (var vent in _hydroThermalVents) {
                 if (vent.IsDiagonal) {
                     if (!useDiagonals) {
-                        // skip
                         continue;
                     }
 
-                    var points = vent.EndCoordinate.x - vent.StartCoordinate.x;
-                    var xBase = vent.StartCoordinate.x;
-                    var isDown = vent.EndCoordinate.y > vent.StartCoordinate.y;
-                    var yEnd = Math.Max(vent.StartCoordinate.y, vent.EndCoordinate.y);
-                    var yStart = Math.Min(vent.StartCoordinate.y, vent.EndCoordinate.y);
-                    for (var i = 0; i <= points; i++) {
-                        if (isDown) {
-                            _floor[yStart + i][xBase + i] += 1;
-                        } else {
-                            _floor[yEnd - i][xBase + i] += 1;
-                        }
-                    }
+                    FillDiagonal(vent);
+                    continue;
                 }
 
                 if (vent.IsHorizontal) {
-                    for (var i = vent.StartCoordinate.x; i <= vent.EndCoordinate.x; i++) {
-                        _floor[vent.StartCoordinate.y][i] += 1;
-                    }
-                } else if (vent.IsVertical) {
-                    for (var i = vent.StartCoordinate.y; i <= vent.EndCoordinate.y; i++) {
-                        _floor[i][vent.StartCoordinate.x] += 1;
-                    }
+                    FillHorizontal(vent);
+                    continue;
                 }
+
+                if (vent.IsVertical) {
+                    FillVertical(vent);
+                }
+            }
+        }
+
+        private void FillVertical(HydroThermalVent vent) {
+            for (var i = vent.StartCoordinate.y; i <= vent.EndCoordinate.y; i++) {
+                _floor[i][vent.StartCoordinate.x] += 1;
+            }
+        }
+
+        private void FillHorizontal(HydroThermalVent vent) {
+            for (var i = vent.StartCoordinate.x; i <= vent.EndCoordinate.x; i++) {
+                _floor[vent.StartCoordinate.y][i] += 1;
+            }
+        }
+
+        private void FillDiagonal(HydroThermalVent vent) {
+            var pointsToFill = vent.EndCoordinate.x - vent.StartCoordinate.x;
+            var isDown = vent.EndCoordinate.y > vent.StartCoordinate.y;
+            var yBase = isDown ? Math.Min(vent.StartCoordinate.y, vent.EndCoordinate.y) : Math.Max(vent.StartCoordinate.y, vent.EndCoordinate.y);
+
+            for (var i = 0; i <= pointsToFill; i++) {
+                // Calculate y
+                var y = yBase;
+                if (isDown) {
+                    y += i;
+                } else {
+                    y -= i;
+                }
+
+                _floor[y][vent.StartCoordinate.x + i] += 1;
             }
         }
 
@@ -76,7 +94,6 @@ public class Day05 : BaseDay {
             _floorBound ??= GetMapBounds();
             _floor = new int[_floorBound.Value.yBound][];
             for (var i = 0; i < _floor.Length; i++) {
-                //_floor[i] = GetVentsOnRow(i);
                 _floor[i] = new int[_floorBound.Value.xBound];
             }
         }
@@ -121,6 +138,7 @@ public class Day05 : BaseDay {
             StartCoordinate = (splits[0][0], splits[0][1]);
             EndCoordinate = (splits[1][0], splits[1][1]);
 
+            // Switch them to order them if necessary
             if ((IsHorizontal || IsDiagonal) && StartCoordinate.x > EndCoordinate.x || IsVertical && StartCoordinate.y > EndCoordinate.y) {
                 (EndCoordinate, StartCoordinate) = (StartCoordinate, EndCoordinate);
             }
