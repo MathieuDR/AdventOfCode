20b77662cbe92b79753bf9b181fd18094755e0e1
Mathieu De Raedt <mathieu@deraedt.dev>
2021-12-06 12:27:15 +0100

    small refactor

--- Year_2021/Day06.cs
+++ Year_2021/Day06.cs
@@ -15,17 +15,24 @@ public class Day06 : BaseDay {
     }
 
     public override ValueTask<string> Solve_1() {
-        var children = Calculate(80);
+        var children = PassDays(80);
         return new ValueTask<string>($"Result: `{children}`");
     }
 
-    private long Calculate(int days) {
+    public override ValueTask<string> Solve_2() {
+        var children = PassDays(256 - 80);
+        return new ValueTask<string>($"Result: `{children}`");
+    }
+
+    private long PassDays(int days) {
         for (var i = 0; i < days; i++) {
             for (var j = 0; j < groups.Count; j++) {
                 if (groups[j].ageGroup == 0) {
+                    // handle offspring
                     groups[9] = (9, groups[j].amount);
                     groups[7] = (7, groups[7].amount + groups[j].amount);
                 } else {
+                    // switch groups
                     groups[j - 1] = (groups[j].ageGroup - 1, groups[j].amount);
                     groups[j] = (j, 0);
                 }
@@ -33,10 +40,5 @@ public class Day06 : BaseDay {
         }
 
         return groups.Sum(x => x.amount);
-    }
-
-    public override ValueTask<string> Solve_2() {
-        var children = Calculate(256 - 80);
-        return new ValueTask<string>($"Result: `{children}`");
     }
 }
