7de17b2604a60c901602da76f0800eff5fba1f8f
Mathieu De Raedt <mathieu@deraedt.dev>
2021-12-10 10:48:44 +0100

    Part 2 day 10

--- Year_2021/Day10.cs
+++ Year_2021/Day10.cs
@@ -4,13 +4,31 @@ namespace AdventOfCode.Year_2021;
 ///     Day 10 from year 2021
 /// </summary>
 public class Day10 : BaseDay {
-    private readonly string[] _lines;
-    private readonly char[] _openCharacters = { '(', '[', '{', '<' };
     private readonly char[] _endCharacters = { ')', ']', '}', '>' };
+    private readonly char[][] _illegalChars;
+    private readonly string[] _lines;
+    private readonly string[] _nonIllegalLines;
+    private readonly char[] _openCharacters = { '(', '[', '{', '<' };
 
 
     public Day10() {
         _lines = File.ReadAllLines(InputFilePath);
+
+        // Set vars
+        var illegalChars = new List<char[]>();
+        var goodLines = new List<string>();
+        foreach (var line in _lines) {
+            var illegal = GetIllegalCharacters(line).ToArray();
+            if (illegal.Length > 0) {
+                illegalChars.Add(illegal);
+                continue;
+            }
+
+            goodLines.Add(line);
+        }
+
+        _illegalChars = illegalChars.ToArray();
+        _nonIllegalLines = goodLines.ToArray();
     }
 
     private IEnumerable<char> GetIllegalCharacters(string input) {
@@ -43,17 +61,58 @@ public class Day10 : BaseDay {
         }
     }
 
-    public override ValueTask<string> Solve_1() {
-        var result = 0;
-        foreach (var line in _lines) {
-            var firstIllegal = GetIllegalCharacters(line).FirstOrDefault();
-            result += GetValueFromCharacter(firstIllegal);
+    private string GetLeftoversString(string input) {
+        var openCharacter = GetOpenCharacters(input);
+        var reversed = openCharacter.Reverse().ToArray();
+        var result = GetCloseCharacterTwin(reversed).ToArray();
+
+
+        return new string(result);
+    }
+
+    private IEnumerable<char> GetCloseCharacterTwin(char[] reversed) {
+        foreach (var c in reversed) {
+            var indexOfEndChar = Array.IndexOf(_openCharacters, c);
+            yield return _endCharacters[indexOfEndChar];
+        }
+    }
+
+    private char[] GetOpenCharacters(string input) {
+        var openCharacter = new List<char>();
+
+        foreach (var character in input) {
+            if (_openCharacters.Contains(character)) {
+                openCharacter.Add(character);
+                continue;
+            }
+
+            var indexOfEndChar = Array.IndexOf(_endCharacters, character);
+            var popped = openCharacter.Last();
+            if (popped == _openCharacters[indexOfEndChar]) {
+                openCharacter.RemoveAt(openCharacter.Count - 1);
+            }
         }
 
+        return openCharacter.ToArray();
+    }
+
+    public override ValueTask<string> Solve_1() {
+        var result = 0;
+
+        foreach (var chars in _illegalChars) {
+            result += GetValueFromIllegalCharacter(chars.First());
+        }
+
+        // `358737`
         return new ValueTask<string>($"Result: `{result}`");
     }
 
-    private int GetValueFromCharacter(char firstIllegal) {
+    private ulong GetAutoCompleteWinner(IEnumerable<ulong> scores) {
+        var arr = scores.OrderBy(x => x).ToArray();
+        return arr[arr.Length / 2];
+    }
+
+    private int GetValueFromIllegalCharacter(char firstIllegal) {
         return firstIllegal switch {
             ')' => 3,
             ']' => 57,
@@ -63,8 +122,31 @@ public class Day10 : BaseDay {
         };
     }
 
+    private ulong GetValueFromIncompleteString(string autoComplete) {
+        ulong result = 0;
+        foreach (var character in autoComplete) {
+            result *= 5;
+            result += GetValueFromIncompleteCharacter(character);
+        }
+
+        return result;
+    }
+
+    private uint GetValueFromIncompleteCharacter(char firstIllegal) {
+        return firstIllegal switch {
+            ')' => 1,
+            ']' => 2,
+            '}' => 3,
+            '>' => 4,
+            _ => 0
+        };
+    }
+
     public override ValueTask<string> Solve_2() {
-        var result = 0;
+        var autoCompletes = _nonIllegalLines.Select(GetLeftoversString).ToArray();
+
+        var autoCompleteScores = autoCompletes.Select(GetValueFromIncompleteString);
+        var result = GetAutoCompleteWinner(autoCompleteScores);
         return new ValueTask<string>($"Result: `{result}`");
     }
 }
