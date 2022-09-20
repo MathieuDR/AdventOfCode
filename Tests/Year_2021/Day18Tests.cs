105b936241c50ec2050ca75c31e7e13e41b46c5e
Mathieu De Raedt <mathieu@deraedt.dev>
2022-09-20 12:19:28 +0200

    Parsing seems to work

--- Tests/Year_2021/Day18Tests.cs
+++ Tests/Year_2021/Day18Tests.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using AdventOfCode.Year_2021;
-using FluentAssertions.Equivalency;
 
 namespace Tests.Year_2021;
 
@@ -15,9 +12,9 @@ public sealed class Day18Tests {
         var result = Day18.Reader.Read(input);
 
         //Assert
-        result.Should().BeEquivalentTo(new Day18.Node(new Day18.Literal(1), new Day18.Literal(2)));
+        result.Should().BeEquivalentTo(new Day18.Node(new Day18.Literal(1, 0), new Day18.Literal(2, 0), 0));
     }
-    
+
     [Fact]
     public void Reader_ShouldParseNode_WhenGivenDoubleToTheLeft() {
         //Arrange
@@ -27,9 +24,10 @@ public sealed class Day18Tests {
         var result = Day18.Reader.Read(input);
 
         //Assert
-        result.Should().BeEquivalentTo(new Day18.Node( new Day18.Node(new Day18.Literal(1), new Day18.Literal(2)),new Day18.Literal(3)));
+        result.Should()
+            .BeEquivalentTo(new Day18.Node(new Day18.Node(new Day18.Literal(1, 1), new Day18.Literal(2, 1), 1), new Day18.Literal(3, 0), 0));
     }
-    
+
     [Fact]
     public void Reader_ShouldParseNode_WhenGivenDoubleToTheRight() {
         //Arrange
@@ -39,6 +37,22 @@ public sealed class Day18Tests {
         var result = Day18.Reader.Read(input);
 
         //Assert
-        result.Should().BeEquivalentTo(new Day18.Node(new Day18.Literal(1), new Day18.Node(new Day18.Literal(2), new Day18.Literal(3))));
+        result.Should()
+            .BeEquivalentTo(new Day18.Node(new Day18.Literal(1, 0), new Day18.Node(new Day18.Literal(2, 1), new Day18.Literal(3, 1), 1), 0));
+    }
+    
+    [Fact]
+    public void Reader_ShouldParseNode_WhenGienComplexNode() {
+        //Arrange
+        var input = "[[1,9],[8,5]]";
+        var expected = new Day18.Node(new Day18.Node(new Day18.Literal(1, 1), new Day18.Literal(9, 1), 1),
+            new Day18.Node(new Day18.Literal(8, 1), new Day18.Literal(5, 1), 1), 0);
+
+        //Act
+        var result = Day18.Reader.Read(input);
+
+        //Assert
+        result.Should()
+            .BeEquivalentTo(expected);
     }
 }
--- Year_2021/Day18.cs
+++ Year_2021/Day18.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text.RegularExpressions;
 using Spectre.Console.Rendering;
 
@@ -23,8 +24,35 @@ internal sealed class Day18 : BaseDay {
     }
     
     internal static class Reader {
-        public static INode Read(string input) {
-            return new Node(new Literal(1), new Literal(1));
+        public static INode Read(string input) => Read(input.AsSpan(), 0, out _);
+        private static INode Read(ReadOnlySpan<char> input, int level, out ReadOnlySpan<char> leftOver) {
+            INode left = null, right = null;
+            var firstChar = input.Slice(1, 1);
+            if (firstChar[0] == '[') {
+                left = Read(input.Slice(1), level + 1, out input);
+            }else if (int.TryParse(firstChar, NumberStyles.Integer, CultureInfo.InvariantCulture, out int literal)) {
+                left = new Literal((short)literal, level);
+                input = input.Slice(2); // remove the number
+            } else {
+                throw new Exception("Unexpected char: " + firstChar[0]);
+            }
+
+            if (input[0] != ',') {
+                throw new Exception("expecting, found: " + firstChar[0]);
+            }
+            
+            var secondChar = input.Slice(1, 1);
+            if (secondChar[0] == '[') {
+                right = Read(input.Slice(1), level + 1, out input);
+            }else if (int.TryParse(secondChar, NumberStyles.Integer, CultureInfo.InvariantCulture, out int literal)) {
+                right = new Literal((short)literal, level);
+                input = input.Slice(2); // remove the number
+            } else {
+                throw new Exception("Unexpected char: " + secondChar[0]);
+            }
+
+            leftOver = input.Slice(1); // remove the ]
+            return new Node(left, right, level);
         }
     }
 
@@ -32,12 +60,12 @@ internal sealed class Day18 : BaseDay {
         public INode? Left { get; }
         public INode? Right { get; }
         public int Level { get; }
-        public INode? Parent { get; }
+        // public INode? Parent { get; }
     }
 
-    internal sealed record Node(INode Left, INode Right, int Level, INode Parent) : INode { }
+    internal sealed record Node(INode Left, INode Right, int Level) : INode { }
 
-    internal sealed record Literal(long Value, int Level, INode Parent) : INode {
+    internal sealed record Literal(short Value, int Level) : INode {
         public INode? Left => null;
         public INode? Right => null;
     }
