c0f9797bea711c2f0c323752fbfc730ba29c79b8
Mathieu De Raedt <mathieu@deraedt.dev>
2022-09-27 23:42:21 +0200

    find common beacons

--- Tests/Year_2021/Day19Tests.cs
+++ Tests/Year_2021/Day19Tests.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Numerics;
 using AdventOfCode.Year_2021;
+using Xunit.Abstractions;
 using Vector = MathNet.Numerics.LinearAlgebra.Single.Vector;
 
 namespace Tests.Year_2021; 
 
 public sealed class Day19Tests {
+    private readonly ITestOutputHelper _helper;
     Func<string, Day19.Scanner[]> ReadScanners = Day19.Reader.ReadScanners;
 
     [Fact]
@@ -106,5 +111,86 @@ public sealed class Day19Tests {
         //Assert
         result[0].Beacons[0].Location.Should().BeEquivalentTo(new Day19.Vector(404, -588, -901));
         result[1].Beacons[8].Location.Should().BeEquivalentTo(new Day19.Vector(-460, 603, 452));
+    }
+    
+    public float ConvertToRadians(float angle)
+    {
+        return (float)((Math.PI / 180) * angle);
+    }
+
+    public Day19Tests(ITestOutputHelper helper) {
+        _helper = helper;
+    }
+
+    [Fact]
+    public void Test() {
+        //Arrange
+        var x = 10;
+        var y = 50;
+        var z = 100;
+        var v1 = new Vector3(x, y, z);
+        
+        //Act
+        var turn = 1.5708f;
+
+        var result = new List<(int xRotation, int yRotation, int zRotation, string coords)>();
+        var xQuat = Quaternion.CreateFromYawPitchRoll(0, 0, turn);
+        var yQuat = Quaternion.CreateFromYawPitchRoll(0, turn, 0);
+        var zQuat = Quaternion.CreateFromYawPitchRoll(turn, 0, 0);
+
+        var rotated = v1;
+        for (int i = 0; i < 4; i++) {
+            if(i>0)
+                rotated = Vector3.Transform(rotated, xQuat);
+            for (int j = 0; j < 4; j++) {
+                if(j>0)
+                    rotated = Vector3.Transform(rotated, yQuat);
+                for (int k = 0; k < 4; k++) {
+                    if(k>0)
+                        rotated = Vector3.Transform(rotated, zQuat);
+                    
+                    result.Add((i, j, k, $"{FindAxis(v1, rotated, 0)}, {FindAxis(v1, rotated, 1)}, {FindAxis(v1, rotated, 2)}"));
+                }
+            }
+        }
+        
+        _helper.WriteLine("Total rotations: " + result.Count);
+        var distinct = result.DistinctBy(t => t.coords).ToList();
+        _helper.WriteLine("distinct rotations: " + distinct.Count);
+        foreach (var (xRotation, yRotation, zRotation, coords) in distinct) {
+            _helper.WriteLine($"Rotation {xRotation}.{yRotation}.{zRotation}: {coords}");
+        }
+
+    }
+
+    private string FindAxis(Vector3 original, Vector3 rotated, int axis) {
+        var tolerance = 0.05f;
+        var value = axis switch {
+            0 => rotated.X,
+            1 => rotated.Y,
+            2 => rotated.Z,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        var sign = value switch {
+            < 0 => "-",
+            _ => ""
+        };
+
+        value = Math.Abs(value);
+
+        if (value - tolerance <= original.X && value + tolerance >= original.X) {
+            return $"{sign}x";
+        }
+        
+        if (value - tolerance <= original.Y && value + tolerance >= original.Y) {
+            return $"{sign}y";
+        }
+        
+        if (value - tolerance <= original.Z && value + tolerance >= original.Z) {
+            return $"{sign}z";
+        }
+
+        throw new ArgumentOutOfRangeException();
     }
 }
--- Year_2021/Day19.cs
+++ Year_2021/Day19.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.Globalization;
+using System.Numerics;
 using AdventOfCode.Common.Helpers;
 
 namespace AdventOfCode.Year_2021;
@@ -7,8 +9,117 @@ namespace AdventOfCode.Year_2021;
 ///     Day 19 from year 2021
 /// </summary>
 internal sealed class Day19 : BaseDay {
+    // private ArrayAdjacencyGraph<Beacon, UndirectedEdge<Beacon>>[] _graphs;
+    private Scanner[] _scanners;
     public Day19() {
+        
         var input = File.ReadAllText(InputFilePath);
+        _scanners = Reader.ReadScanners(input);
+
+        var sw = Stopwatch.StartNew();
+        
+        var distances =
+            new List<Dictionary<Beacon, Dictionary<Beacon, float>>>();
+        
+        
+        CreateDistanceDictionaries(distances);
+        
+        sw.Stop();
+        
+        Console.WriteLine($"Done indexing in {sw.ElapsedMilliseconds}ms with");
+        sw.Start();
+        var beaconField = CreateCommonBeaconField(distances);
+        sw.Stop();
+        
+        Console.WriteLine(beaconField.Count);
+    }
+
+    private static List<Beacon> CreateCommonBeaconField(List<Dictionary<Beacon, Dictionary<Beacon, float>>> distances) {
+        const int neededToBeCommon = 12;
+        const int neededDistances = 1;
+        var beaconField = distances[0];
+        var scannerList = distances.Skip(1).ToList();
+        var result = beaconField.Select(x => x.Key).ToList();
+        
+        while (scannerList.Any()) {
+            Dictionary<Beacon, Dictionary<Beacon, float>>? field = null;
+            foreach (var toTest in scannerList) {
+                var commonBeacons = new List<Beacon>();
+                foreach (var (beacon, beaconDistances) in toTest) {
+                    if (commonBeacons.Contains(beacon)) {
+                        continue;
+                    }
+                    foreach (var (toBeacon, distance) in beaconDistances) {
+                        if (commonBeacons.Contains(toBeacon)) {
+                            continue;
+                        }
+                        
+                        var equalDistances = 0;
+                        foreach (var (_, consolidatedBeaconDistances) in beaconField) {
+                            foreach (var (_, consolidatedDistance) in consolidatedBeaconDistances) {
+                                if (Math.Abs(consolidatedDistance - distance) < 0.01f) {
+                                    // Equals
+                                    equalDistances++;
+                                }
+
+                                if (equalDistances >= neededDistances) {
+                                    break;
+                                }
+                            }
+                            
+                            if (equalDistances >= neededDistances) {
+                                break;
+                            }
+                        }
+                        
+                        if (equalDistances >= neededDistances) {
+                            commonBeacons.Add(beacon);
+                            
+                            //if(!commonBeacons.Contains(toBeacon))
+                                commonBeacons.Add(toBeacon);
+                            break;
+                        }
+                    }
+                }
+
+                
+                //commonBeacons = commonBeacons.Distinct().ToList();
+
+                if (commonBeacons.Count >= neededToBeCommon) {
+                    field = toTest;
+                    break;
+                }
+
+                Console.WriteLine($"Scanner not OK only found {commonBeacons.Count}");
+            }
+
+            if (field is null) {
+                throw new Exception("Couldn't find field");
+            }
+
+            // add to my beaconfield
+            scannerList.Remove(field);
+        }
+
+        return result;
+    }
+
+    private void CreateDistanceDictionaries(List<Dictionary<Beacon, Dictionary<Beacon, float>>> distances) {
+        foreach (var scanner in _scanners) {
+            var dict = new Dictionary<Beacon, Dictionary<Beacon, float>>();
+            for (var i = 0; i < scanner.Beacons.Length - 1; i++) {
+                var fromBeacon = scanner.Beacons[i];
+                var beaconDict = new Dictionary<Beacon, float>();
+                for (var u = i + 1; u < scanner.Beacons.Length; u++) {
+                    var toBeacon = scanner.Beacons[u];
+                    beaconDict.Add(toBeacon, VectorHelper.DistanceFrom(fromBeacon.Location, toBeacon.Location));
+                }
+
+                dict.Add(fromBeacon, beaconDict);
+            }
+
+            distances.Add(dict);
+        }
     }
 
     public override ValueTask<string> Solve_1() {
@@ -64,8 +175,11 @@ internal sealed class Day19 : BaseDay {
     internal sealed record Vector(int X, int Y, int Z);
 
     internal static class VectorHelper {
-        public static Vector DistanceFrom(Vector from, Vector to) {
-            return new Vector(from.X - to.X, from.Y - to.Y, from.Z - to.Z);
+        private static Vector3 ToVector3(Vector vector) => new Vector3(vector.X, vector.Y, vector.Z);
+        public static float DistanceFrom(Vector from, Vector to) {
+            var from3 = ToVector3(from);
+            var to3 = ToVector3(to);
+            return Vector3.Distance(from3, to3);
         }
     }
 
